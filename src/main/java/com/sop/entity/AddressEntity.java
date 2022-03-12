fd113fc4bb91a5d572cecb9a682a668ae9a5b073
Dariusz Głażewski <szprx1910@gmail.com>
2022-03-12 15:13:38 +0100

    [author]Dariusz Glazewski [trello]SOP-5 [comment]Dodanie klasy AddressEntity

--- src/main/java/com/sop/entity/AddressEntity.java
+++ src/main/java/com/sop/entity/AddressEntity.java
@@ -1,4 +1,37 @@
 package com.sop.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ADDRESS")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
 public class AddressEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String city;
+    private String street;
+    private String streetNumber;
+    private String postalCode;
+    private String country;
+    private double lat;
+    private double lng;
+
 }
+
+
