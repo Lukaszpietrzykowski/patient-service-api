74fb30c771a73b7db13901e58b5b9df57f22c7ff
No Ja <redokoks9944@gmail.com>
2022-03-12 16:07:14 +0100

    Sop 4 (#12)
    * Jakiś tekst
    
    * [author]:Radosław Michał Piwko
    [trello]:SOP-4
    [comment]:Utworzylem klase HospitalEntity i relacje do Id w AddressEntity
    
    Co-authored-by: Radek <s200861@sggw.edu.pl>

---
+++ src/main/java/com/sop/entity/HospitalEntity.java
@@ -0,0 +1,37 @@
+package com.sop.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "HOSPITAL")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class HospitalEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @OneToOne
+    @JoinColumn(name = "ADDRESS", referencedColumnName = "ID")
+    private AddressEntity address;
+
+
+    private Long departments;
+
+}
