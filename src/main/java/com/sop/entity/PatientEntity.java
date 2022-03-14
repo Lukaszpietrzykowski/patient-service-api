267528dc77b59371fb4eac4d05b2304d57fc60f4
piotrtag1 <piotrektag22@gmail.com>
2022-03-14 19:56:23 +0100

    [Piotr Tag] [SOP-3] [Dodanie Relacji miedzy tabelami DepartmentEntity a PatientEntity, poprawa nazwy tabeli, dodanie relacji odwrotnej w tabeli PatientEntity zgodnie z poleceniem]

--- src/main/java/com/sop/entity/PatientEntity.java
+++ src/main/java/com/sop/entity/PatientEntity.java
@@ -6,13 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
@@ -39,4 +33,7 @@ public class PatientEntity {
 
     private LocalDateTime dischargeDate;
     private LocalDateTime registrationDate;
+
+    @ManyToOne
+    DepartmentEntity department;
 }
