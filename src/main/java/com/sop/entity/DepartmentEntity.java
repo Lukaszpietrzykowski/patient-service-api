8d61445f629e360b14d4a2f3a215c317a8d8160b
Patryk Ostrowski <ostrowskipatryk2000@gmail.com>
2022-03-20 15:03:04 +0100

    [author] Patryk Ostrowski [trello] SOP-3_1 [comment] Dodanie relacji między tabelami depatramen i hospital relacja OneToMany.

--- src/main/java/com/sop/entity/DepartmentEntity.java
+++ src/main/java/com/sop/entity/DepartmentEntity.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -22,7 +23,11 @@ public class DepartmentEntity {
     private String name;
 
     @OneToMany(mappedBy = "department")
-    private List<PatientEntity> patients;
+    private List<PatientEntity> patients = new ArrayList<>() ;
+
+    @ManyToOne
+    @JoinColumn(name = "hospital_id")
+    private HospitalEntity hospital;
 
     private long availableBeds;
 }
