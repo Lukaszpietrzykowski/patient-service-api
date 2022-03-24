7788ae21ef967121b2a70b22d7f10d9ca52b79fd
Patryk Ostrowski <ostrowskipatryk2000@gmail.com>
2022-03-24 21:56:36 +0100

    [author] Patryk Ostrowski [trello] SOP-14 [comment] Utworzenie klasy PatientService

---
+++ src/main/java/com/sop/service/PatientService.java
@@ -0,0 +1,23 @@
+package com.sop.service;
+
+import com.sop.dto.PatientDto;
+import com.sop.repository.PatientRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class PatientService {
+
+    private final PatientRepository repository;
+
+    public List<PatientDto> getPatients() {
+        return repository.findAll()
+                .stream()
+                .map(PatientDto::of)
+                .toList();
+    }
+
+}
