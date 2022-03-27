edd6f5a3384fd76efa756a5392ae6e8db70ba148
piotrtag1 <piotrektag22@gmail.com>
2022-03-27 17:11:36 +0200

    [Piotr Tag] [SOP-08] [Dodanie DepartmentRepository]

--- src/main/java/com/sop/repository/DepartmentRepository.java
+++ src/main/java/com/sop/repository/DepartmentRepository.java
@@ -1,9 +1,9 @@
 package com.sop.repository;
 
-import com.sop.entity.PatientEntity;
+import com.sop.entity.DepartmentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DepartmentRepository extends JpaRepository<DepartmentRepository,Long> {
+public interface DepartmentRepository extends JpaRepository<DepartmentEntity,Long> {
 }
