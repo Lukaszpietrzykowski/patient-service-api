9050e4fa24ee647e8d5f102b646fbe5dbfbfed9e
Radek <s200861@sggw.edu.pl>
2022-03-29 00:40:51 +0200

    [author]:Radosław Michał Piwko [trello]:SOP-11 [comment]:Utworzyłem HospitalRepository

---
+++ src/main/java/com/sop/repository/HospitalRepository.java
@@ -0,0 +1,8 @@
+package com.sop.repository;
+
+import com.sop.entity.HospitalEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface HospitalRepository extends JpaRepository<HospitalEntity, Long> {
+}
