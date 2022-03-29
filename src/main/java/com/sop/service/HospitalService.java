5db7fbd34fa2d761326fb0f8da0b372a3f56c335
Radek <s200861@sggw.edu.pl>
2022-03-29 19:27:26 +0200

    [author]:Radosław Michał Piwko [trello]:SOP-15 [comment]:Utworzyłem HospitalService

---
+++ src/main/java/com/sop/service/HospitalService.java
@@ -0,0 +1,21 @@
+package com.sop.service;
+
+import com.sop.dto.HospitalDto;
+import com.sop.repository.HospitalRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import  java.util.List;
+@Service
+@AllArgsConstructor
+public class HospitalService {
+
+    HospitalRepository hospitalRepository;
+
+    public List<HospitalDto> getHospitals(){
+        return hospitalRepository.findAll()
+                .stream()
+                .map(HospitalDto::of)
+                .toList();
+    }
+}
