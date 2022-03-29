e958facf52eed9fdcd079c135171c97d6e7e1624
Radek <s200861@sggw.edu.pl>
2022-03-29 16:22:42 +0200

    [author]:Radosław Michał Piwko [trello]:SOP-20 [comment]:Utworzyłem HospitalDto

---
+++ src/main/java/com/sop/dto/HospitalDto.java
@@ -0,0 +1,32 @@
+package com.sop.dto;
+
+import com.sop.entity.AddressEntity;
+import com.sop.entity.HospitalEntity;
+import com.sop.entity.DepartmentEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+import java.util.ArrayList;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class HospitalDto {
+    private long id;
+    private String name;
+    private AddressEntity address;
+    private List<DepartmentEntity> deparments;
+    public static HospitalDto of(HospitalEntity hospital)
+    {
+        return HospitalDto.builder()
+                .id(hospital.getId())
+                .name(hospital.getName())
+                .address(hospital.getAddress())
+                .deparments(hospital.getDepartments())
+                .build();
+    }
+}
