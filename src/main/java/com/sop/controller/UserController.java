e8be3ea9a4235a57a402a7742ca9f90cdde9fb77
Kacper Michałek <Kacpral@wp.pl>
2022-03-30 21:54:41 +0200

    [author] Kacper Michałek [trello] SOP-21 [comment] UserController nie wiedziałem pod jaki url ustawić mapowanie

---
+++ src/main/java/com/sop/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.sop.controller;
+
+import com.sop.dto.UserDto;
+import com.sop.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/user")
+
+public class UserController {
+
+    private UserService userService;
+
+    @GetMapping("/getAll")
+    public List<UserDto> getUserList(){
+    return userService.getUsers();
+    }
+
+}
