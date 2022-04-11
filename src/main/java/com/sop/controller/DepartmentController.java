3a0f5af530300094314b984a747cd40a024a9dce
piotrtag1 <piotrektag22@gmail.com>
2022-04-11 06:25:56 -0700

    [SOP-27] [Piotr Tag] [Dodanie funkcjonalnosci CRUD]

--- src/main/java/com/sop/controller/DepartmentController.java
+++ src/main/java/com/sop/controller/DepartmentController.java
@@ -1,11 +1,11 @@
 package com.sop.controller;
 
 import com.sop.dto.DepartmentDto;
+import com.sop.entity.DepartmentEntity;
 import com.sop.service.DepartmentService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,11 +13,46 @@ import java.util.List;
 @AllArgsConstructor
 @RequestMapping("/department")
 public class DepartmentController {
-
+    @Autowired
     private final DepartmentService departmentService;
 
     @GetMapping
     public List<DepartmentDto> getDepartment() {
         return departmentService.getDepartment();
+    }
+
+    @PostMapping("/department/addDepartment")
+    public DepartmentEntity addDepartment(@RequestBody DepartmentEntity departmentEntity)
+    {
+        return departmentService.saveDepartment(departmentEntity);
+    }
+
+    @PostMapping("/department/addDepartments")
+    public List<DepartmentEntity> addDepartments(@RequestBody List<DepartmentEntity> departmentEntities)
+    {
+        return departmentService.saveDepartments(departmentEntities);
+    }
+    @GetMapping("/department/departmentById/{departmentId}")
+    public DepartmentEntity findDepartmentById(@PathVariable long departmentId)
+    {
+        return departmentService.getDepartmentById(departmentId);
+    }
+
+    @GetMapping("/department/departments")
+    public List<DepartmentEntity> findAllDepartments()
+    {
+        return departmentService.getDepartmests();
+    }
+
+    @PutMapping("/department/update")
+    public DepartmentEntity updateDepartment(@RequestBody DepartmentEntity departmentEntity)
+    {
+        return departmentService.updateDepartment(departmentEntity);
+    }
+
+    @DeleteMapping("/department/dellete/{departmentId}")
+    public void deleteDepartment(@PathVariable long departmentId)
+    {
+        departmentService.deleteDepartment(departmentId);
     }
 }
--- src/main/java/com/sop/service/DepartmentService.java
+++ src/main/java/com/sop/service/DepartmentService.java
@@ -1,8 +1,10 @@
 package com.sop.service;
 
 import com.sop.dto.DepartmentDto;
+import com.sop.entity.DepartmentEntity;
 import com.sop.repository.DepartmentRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,8 +12,43 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class DepartmentService {
-
+    @Autowired
     private final DepartmentRepository repository;
+
+    public DepartmentEntity saveDepartment(DepartmentEntity departmentEntity)
+    {
+        return repository.save(departmentEntity);
+    }
+
+    public List<DepartmentEntity> saveDepartments(List<DepartmentEntity> departmentEntities)
+    {
+        return repository.saveAll(departmentEntities);
+    }
+
+    public List<DepartmentEntity> getDepartmests()
+    {
+        return repository.findAll();
+    }
+
+    public DepartmentEntity getDepartmentById(long departmentId)
+    {
+        return repository.findById(departmentId).orElse(null);
+    }
+
+    public void deleteDepartment(long departmentId)
+    {
+        repository.deleteById(departmentId);
+    }
+
+    public DepartmentEntity updateDepartment(DepartmentEntity departmentEntity)
+    {
+        DepartmentEntity existingDepartment = repository.findById(departmentEntity.getId()).orElse(null);
+        existingDepartment.setName(departmentEntity.getName());
+        existingDepartment.setHospital(departmentEntity.getHospital());
+        existingDepartment.setAvailableBeds(departmentEntity.getAvailableBeds());
+
+        return repository.save(existingDepartment);
+    }
 
     public List<DepartmentDto> getDepartment() {
         return repository.findAll()
