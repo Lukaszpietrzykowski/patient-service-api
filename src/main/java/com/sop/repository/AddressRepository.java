66690ef70b70528ace835d38e13d7f901900d90f
Dariusz Głażewski <szprx1910@gmail.com>
2022-04-12 17:30:43 +0200

    [author] Dariusz Glazewski [trello] SOP-28 [comment] dodalem zapobieganie dodawania duplikatow

--- src/main/java/com/sop/repository/AddressRepository.java
+++ src/main/java/com/sop/repository/AddressRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepository extends JpaRepository<AddressEntity,Long> {
+    boolean existsByCityAndStreetAndStreetNumberAndPostalCodeAndCountryAndLatAndLng(String city, String street, String streetNumber, String postalCode, String country, double lat, double lng);
 }
--- src/main/java/com/sop/service/AddressService.java
+++ src/main/java/com/sop/service/AddressService.java
@@ -5,6 +5,7 @@ import com.sop.dto.AddressDto;
 import com.sop.entity.AddressEntity;
 import com.sop.repository.AddressRepository;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,11 @@ public class AddressService {
 
     private final ModelMapper modelMapper;
 
+    @SneakyThrows
     public AddressDto addAddress(AddressCreator addressCreator) {
+        if (exists(addressCreator)) {
+            throw new Exception("Already added");
+        }
         return convertToDto(addressRepository.save(convertToEntity(addressCreator)));
     }
 
@@ -29,21 +34,38 @@ public class AddressService {
                 .toList();
     }
 
+    @SneakyThrows
     public AddressDto getAddress(Long id) {
-        return convertToDto(addressRepository.findById(id).orElseThrow());
+        return convertToDto(addressRepository.findById(id)
+                .orElseThrow());
     }
 
+    @SneakyThrows
     public AddressDto updateAddress(Long id, AddressCreator addressCreator) {
+        if (exists(addressCreator)) {
+            throw new Exception("Already added");
+        }
         return convertToDto(addressRepository.findById(id)
                 .map(oldAddress -> {
-                            AddressEntity updatedAddress = oldAddress.updateWith(convertToEntity(addressCreator));
-                            return addressRepository.save(updatedAddress);
-                        }
-                ).orElseThrow());
+                    AddressEntity updatedAddress = oldAddress.updateWith(convertToEntity(addressCreator));
+                    return addressRepository.save(updatedAddress);
+                })
+                .orElseThrow());
     }
 
     public void deleteAddress(Long id) {
         addressRepository.deleteById(id);
+    }
+
+    private boolean exists(AddressCreator addressCreator) {
+        return addressRepository.existsByCityAndStreetAndStreetNumberAndPostalCodeAndCountryAndLatAndLng(
+                addressCreator.getCity(),
+                addressCreator.getStreet(),
+                addressCreator.getStreetNumber(),
+                addressCreator.getPostalCode(),
+                addressCreator.getCountry(),
+                addressCreator.getLat(),
+                addressCreator.getLng());
     }
 
     private AddressEntity convertToEntity(AddressCreator addressCreator) {
