2a918fb12ae6b895fa55caafa6d2cb65d20d1148
Dariusz Głażewski <szprx1910@gmail.com>
2022-04-14 22:24:24 +0200

    [author] Dariusz Glazewski [trello] SOP-28 [comment] usuniecie niepotrzebnej metody

--- src/main/java/com/sop/repository/AddressRepository.java
+++ src/main/java/com/sop/repository/AddressRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AddressRepository extends JpaRepository<AddressEntity,Long> {
-    boolean existsByCityAndStreetAndStreetNumberAndPostalCodeAndCountryAndLatAndLng(String city, String street, String streetNumber, String postalCode, String country, double lat, double lng);
 }
