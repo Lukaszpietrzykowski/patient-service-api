312304a6cd30f72fb5949af5fff22bdeceefb960
Dariusz Głażewski <szprx1910@gmail.com>
2022-04-14 22:21:46 +0200

    [author] Dariusz Glazewski [trello] SOP-28 [comment] usuniecie settera

--- src/main/java/com/sop/creators/AddressCreator.java
+++ src/main/java/com/sop/creators/AddressCreator.java
@@ -1,14 +1,12 @@
-package com.sop.creator;
+package com.sop.creators;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-@Setter
 public class AddressCreator {
     private String city;
     private String street;
