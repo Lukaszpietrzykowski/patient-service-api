633b7b1d4249a82e606c4482c8521bb931968e07
Dariusz Głażewski <szprx1910@gmail.com>
2022-04-14 22:23:47 +0200

    [author] Dariusz Glazewski [trello] SOP-28 [comment] dodanie metod mapujacych obiekty

--- src/main/java/com/sop/entity/AddressEntity.java
+++ src/main/java/com/sop/entity/AddressEntity.java
@@ -1,9 +1,10 @@
 package com.sop.entity;
 
+import com.sop.creators.AddressCreator;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,7 +17,7 @@ import javax.persistence.Table;
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
-@Setter
+@Builder
 public class AddressEntity {
 
     @Id
@@ -31,17 +32,27 @@ public class AddressEntity {
     private double lat;
     private double lng;
 
+    public static AddressEntity of(AddressCreator address) {
+        return AddressEntity.builder()
+                .city(address.getCity())
+                .street(address.getStreet())
+                .streetNumber(address.getStreetNumber())
+                .postalCode(address.getPostalCode())
+                .country(address.getCountry())
+                .build();
+    }
+
     public AddressEntity updateWith(AddressEntity address) {
-        return new AddressEntity(
-                this.id,
-                address.city,
-                address.street,
-                address.streetNumber,
-                address.postalCode,
-                address.country,
-                address.lat,
-                address.lng);
+        return AddressEntity.builder()
+                .id(this.id)
+                .city(address.getCity())
+                .street(address.getStreet())
+                .streetNumber(address.getStreetNumber())
+                .postalCode(address.getPostalCode())
+                .country(address.getCountry())
+                .lat(address.getLat())
+                .lng(address.getLng())
+                .build();
     }
 }
-
 
