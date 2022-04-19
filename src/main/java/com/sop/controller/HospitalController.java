25db3a3b039c1125fceccc2870d6a1f6a33230d5
No Ja <redokoks9944@gmail.com>
2022-04-19 18:43:51 +0200

    [author]:Radosław Michał Piwko (#38)
    [trello]:SOP-30
    [comment]:Utworzyłem CRUD'a
    
    Co-authored-by: Radek <s200861@sggw.edu.pl>

--- src/main/java/com/sop/controller/HospitalController.java
+++ src/main/java/com/sop/controller/HospitalController.java
@@ -1,11 +1,19 @@
 package com.sop.controller;
 
+import com.sop.creators.HospitalCreator;
+import com.sop.entity.HospitalEntity;
 import com.sop.dto.HospitalDto;
 import com.sop.service.HospitalService;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
@@ -13,12 +21,32 @@ import java.util.List;
 @AllArgsConstructor
 @RequestMapping("/hospital")
 public class HospitalController {
-
+    @Autowired
     private final HospitalService hospitalService;
 
     @GetMapping
     public List<HospitalDto> getHospitalList() {
+
         return hospitalService.getHospitals();
+    }
+
+    @GetMapping("/{id}")
+    public HospitalDto getHospitals(@PathVariable long id){
+        return HospitalService.getHospital(id);
+    }
+
+    @PostMapping("/add")
+    public  HospitalDto addHospital(@RequestBody HospitalCreator hospitalEntity)
+    {
+        return hospitalService.createHospital(hospitalEntity);
+    }
+    @PutMapping("/update/{id}")
+    public HospitalDto updateAddress(@PathVariable Long id, @RequestBody HospitalCreator hospitalCreator){
+        return  hospitalService.updateHospital(id, hospitalCreator);
+    }
+    @DeleteMapping("/delete/{id}")
+    public void deleteHospital(@PathVariable Long id){
+        hospitalService.deleteHospital(id);
     }
 
 }
--- src/main/java/com/sop/service/HospitalService.java
+++ src/main/java/com/sop/service/HospitalService.java
@@ -1,22 +1,47 @@
 package com.sop.service;
 
+import com.sop.creators.HospitalCreator;
 import com.sop.dto.HospitalDto;
 import com.sop.repository.HospitalRepository;
+import com.sop.entity.HospitalEntity;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class HospitalService {
 
-    private final HospitalRepository hospitalRepository;
+
+    public HospitalDto createHospital(HospitalCreator hospitalCreator)
+    {
+        return HospitalDto.of(hospitalRepository.save(HospitalEntity.of(hospitalCreator)));
+    }
+    @Autowired
+    private static HospitalRepository hospitalRepository;
 
     public List<HospitalDto> getHospitals() {
         return hospitalRepository.findAll()
                 .stream()
                 .map(HospitalDto::of)
                 .toList();
+    }
+    public static HospitalDto getHospital(long id){
+        return HospitalDto.of(hospitalRepository.findById(id).orElseThrow());
+
+    }
+    public HospitalDto updateHospital(Long id, HospitalCreator hospitalCreator){
+        return HospitalDto.of(hospitalRepository.findById(id)
+                .map(oldHospital ->{
+                    HospitalEntity updateHospital = oldHospital.updateWith(HospitalEntity.of(hospitalCreator));
+                    return hospitalRepository.save(updateHospital);
+                }).orElseThrow());
+    }
+    public void deleteHospital(long id){
+        hospitalRepository.deleteAllById(Collections.singleton(id));
     }
 }
