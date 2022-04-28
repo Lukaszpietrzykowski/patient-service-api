1497efdd1fe84d587a0851db8e8a517e3f9cce7d
Kacper Michałek <Kacpral@wp.pl>
2022-04-28 23:07:36 +0200

    [author] Kacper Michałek [trello] SOP-31 [comment] Dodanie crud

--- src/main/java/com/sop/controller/UserController.java
+++ src/main/java/com/sop/controller/UserController.java
@@ -1,9 +1,18 @@
 package com.sop.controller;
 
+import com.sop.creators.AddressCreator;
+import com.sop.creators.UserCreator;
+import com.sop.dto.AddressDto;
 import com.sop.dto.UserDto;
 import com.sop.service.UserService;
 import lombok.AllArgsConstructor;
+import org.apache.catalina.User;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +25,28 @@ public class UserController {
 
     private UserService userService;
 
-    @GetMapping
-    public List<UserDto> getUserList() {
+    @GetMapping("/all")
+    public List<UserDto> getUsers() {
         return userService.getUsers();
+    }
+
+    @GetMapping("/{id}")
+    public UserDto getUser(@PathVariable long id) {
+        return userService.getUser(id);
+    }
+
+    @PostMapping("/add")
+    public UserDto createUser(@RequestBody UserCreator user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/update/{id}")
+    public UserDto updateUser(@PathVariable long id, @RequestBody UserCreator user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public void deleteAddress(@PathVariable long id) {
+        userService.deleteUser(id);
     }
 }
--- src/main/java/com/sop/service/UserService.java
+++ src/main/java/com/sop/service/UserService.java
@@ -1,6 +1,11 @@
 package com.sop.service;
 
+import com.sop.creators.AddressCreator;
+import com.sop.creators.UserCreator;
+import com.sop.dto.AddressDto;
 import com.sop.dto.UserDto;
+import com.sop.entity.AddressEntity;
+import com.sop.entity.UserEntity;
 import com.sop.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,6 +23,27 @@ public class UserService {
                 .stream()
                 .map(UserDto::of)
                 .toList();
+    }
+
+    public UserDto createUser(UserCreator userCreator) {
+        return UserDto.of(repository.save(UserEntity.of(userCreator)));
+    }
+    public UserDto getUser(Long id) {
+        return UserDto.of(repository.findById(id)
+                .orElseThrow());
+    }
+
+    public UserDto updateUser(Long id, UserCreator userCreator) {
+        return UserDto.of(repository.findById(id)
+                .map(oldUser -> {
+                    UserEntity updatedUser = oldUser.updateWith(UserEntity.of(userCreator));
+                    return repository.save(updatedUser);
+                })
+                .orElseThrow());
+    }
+
+    public void deleteUser(Long id) {
+        repository.deleteById(id);
     }
 
 }
