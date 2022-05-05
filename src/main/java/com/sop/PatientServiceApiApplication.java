d5de1ff5c43adb25145aec820daad3ffe1a49d2d
Dariusz Głażewski <szprx1910@gmail.com>
2022-05-05 11:55:41 +0200

    [author] Dariusz Glazewski [trello] SOP-32 [comment] dodanie admina do bazy danych,chyba tymczasowe

--- src/main/java/com/sop/PatientServiceApiApplication.java
+++ src/main/java/com/sop/PatientServiceApiApplication.java
@@ -1,13 +1,28 @@
 package com.sop;
 
+import com.sop.creators.UserCreator;
+import com.sop.enums.RoleEnum;
+import com.sop.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PatientServiceApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PatientServiceApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PatientServiceApiApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner demo(UserService userService) {
+        return args -> {
+            String rawPassword = "pass";
+            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+            userService.createUser(new UserCreator("admin", "admin", encoder.encode(rawPassword), RoleEnum.ADMIN));
+        };
+    }
 }
+
